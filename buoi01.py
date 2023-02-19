8a27458d3cefe454369693419c418a6a00e4b7fe
Bagumeow <93193119+Bagumeow@users.noreply.github.com>
2023-02-19 09:40:50 +0700

    Add files via upload

---
+++ buoi01.py
@@ -0,0 +1,28 @@
+# #bai01 
+# print("Duong The Ngoc")
+
+# bai 02
+# firstname = input("Nhap ho: ")
+# lastname = input("Nhap ten: ")
+# fullname = firstname + lastname
+# print(fullname)
+
+#bai 03
+# a = input("Nhap chuoi a: ")
+# b = input("Nhap chuoi b: ")
+# if a in b:
+#     print ("a same b")
+# else:
+#     print ("a khac b")
+
+#bai 04
+arr = ["Ngoc", "Huy", "Thang"]
+add = input("Nhap ten: ")
+for  i in range(0, len(arr)) :
+    print (i)
+if add in arr[i]:
+     print(i)
+ 
+
+
+
